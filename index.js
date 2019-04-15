b4103913f0b3a2f268e577552e3e2e0d64bb308e
aundrae <justinramjattan@outlook.com>
2019-04-15 00:01:43 -0400

    1 file modified, 2 added, 1 deleted

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
 import './style.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import IconButton from '@material-ui/core/IconButton';
-import AppBar from '@material-ui/core/AppBar'
-import ToolBar from '@material-ui/core/Toolbar'
-import MenuIcon from '@material-ui/icons/Menu'
-import Typography from '@material-ui/core/Typography';
-import Drawer from '@material-ui/core/Drawer';
-import ListItem from 'material-ui/ListItem';
-
+import PermanentDrawerLeft from './components/AppBar'
 class App extends Component {
   constructor() {
-    super();
     this.state = {
-      name: 'React'
+      name: 'React',
     };
   }
-
+  
   render() {
     return (
-      <Router>
-        <AppBar style={{ backgroundColor: '#ee0000' }}>
-          <ToolBar>
-            <IconButton color='inherit'>
-              <MenuIcon/>
-            </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              EmployTT
-            </Typography>
-          </ToolBar>
-        </AppBar>
-      </Router>
-    );
+      <h1>style</h1>
+    )
   }
 }
 
